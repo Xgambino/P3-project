2c5d7b73bfce2fd6fd9f7c18f635abf8f94f594a
Denis <mugambimuthuri0@gmail.com>
2024-06-16 18:17:45 +0300

    initial backend commit

--- back-end/app.py
+++ back-end/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
+class CatalogueModel(BaseModel):
+    name: str
+    description: str
+    # price: float
+    # is_offer: Union[bool, None] = None
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/catalogue')
+def get_catalogues():
+    return[{"name":"Yamaha R6"}]
+
+@app.post('/catalogue')
+def save_catalogue(data: CatalogueModel):
+    print(data)
